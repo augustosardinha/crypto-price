215cdbf3a1a3984bed9b5dadc8502a459fc8c205
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-19 21:41:56 -0300

    feat: create App

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from './components/HeaderApp.vue'
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-gradient-to-b from-purple-800 to-purple-600 font-poppins">
+    <Header />
+    <main class="flex flex-col items-center justify-center h-5/6 gap-12">
+      <router-view />
+    </main>
+  </div>
+</template>
