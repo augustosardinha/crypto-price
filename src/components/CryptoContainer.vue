46482e9b5121ac5db352499106a113527ce245e3
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-19 21:45:38 -0300

    feat: create CryptoContainer component

---
+++ src/components/CryptoContainer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex gap-5">
+    <img
+      v-for="crypto in cryptos"
+      :key="crypto.id"
+      class="w-12 h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 cursor-pointer
+        opacity-60 hover:opacity-100 hover:animate-bounce"
+      :src="crypto.source"
+      :alt="crypto.alt"
+      @click="changeCrypto(crypto.id)"
+    >
+  </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+import { mutationsName } from '../store/mutations'
+import { cryptos } from '../utils/cryptos'
+
+const store = useStore()
+const changeCrypto = (crypto) => {
+  store.commit(mutationsName.SET_CURRENT_CRYPTO, crypto)
+  store.commit(mutationsName.SET_COUNTDOWN_INTERVAL)
+}
+</script>
