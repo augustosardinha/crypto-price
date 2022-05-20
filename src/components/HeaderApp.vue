502d534c64e99724ca5b50c4bb2dae18f97133fc
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-19 21:45:07 -0300

    feat: create HeaderApp component

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="h-1/6 flex justify-center shadow-xl">
+    <router-link
+      to="/"
+      class="flex items-center gap-4"
+    >
+      <img
+        class="w-12 md:w-16"
+        src="../assets/dacxi.png"
+        alt="Crypto Price Logo"
+      >
+      <span class="font-bold text-3xl md:text-4xl text-white">Crypto Price</span>
+    </router-link>
+  </header>
+</template>
