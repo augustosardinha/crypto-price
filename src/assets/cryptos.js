26471bba279b00a294d10e8b4ccc2c4309ee8e21
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-19 21:42:29 -0300

    feat: create assets folder for images in application

---
+++ src/assets/cryptos.js
@@ -0,0 +1,31 @@
+import dacxi from '../assets/dacxi.png'
+import ethereum from '../assets/ethereum.png'
+import atom from '../assets/atom.png'
+import luna from '../assets/luna.png'
+import bitcoin from '../assets/bitcoin.png'
+
+export const cryptos = [{
+    id: "bitcoin",
+    source: bitcoin,
+    alt:"bitcoin"
+},
+{
+    id: "dacxi",
+    source: dacxi,
+    alt:"dacxi"
+},
+{
+    id: "ethereum",
+    source: ethereum,
+    alt:"ethereum"
+},
+{
+    id: "cosmos",
+    source: atom,
+    alt:"cosmos"
+},
+{
+    id: "terra-luna",
+    source: luna,
+    alt:"terra"
+}]
