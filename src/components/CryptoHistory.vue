ef4a00f51fcaec8eba3f0303705855b45fa7074a
Augusto Sardinha <ssardinhaaugusto@gmail.com>
2022-05-19 21:47:40 -0300

    feat: create CryptoHistory component

---
+++ src/components/CryptoHistory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex flex-col gap-5">
+    <h2 class="text-white">
+      <span class="font-semibold">{{ currentCrypto.name }}</span> value on
+      <span class="font-semibold">{{ history.date }}</span> :
+    </h2>
+    <h3 class="font-bold text-center text-white text-3xl">
+      {{ history.price }}
+    </h3>
+    <button
+      class="py-3 px-12 font-semibold text-sm  text-white rounded shadow-lg bg-yellow-600 hover:bg-yellow-500"
+      @click="handleClick"
+    >
+      BACK
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+import { mutationsName } from '../store/mutations'
+
+const store = useStore()
+const history = computed(() => store.getters.getHistory)
+const currentCrypto = computed(() => store.getters.getCurrentCrypto)
+
+const handleClick = () => store.commit(mutationsName.CLEAR_HISTORY)
+
+</script>
